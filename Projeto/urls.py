057e4ec0313bf266d044f682b54bace6f82de323
D <gabriel001.contato@gmail.com>
2023-11-26 16:32:14 -0300

    HTML+1

--- Projeto/urls.py
+++ Projeto/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaAlunosMatriculados, LoginPage
 from rest_framework import routers
+from django.views.generic import RedirectView
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
@@ -11,6 +12,7 @@ router.register('matriculas', MatriculasViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginPage.as_view(), name='login'),
     path('', include(router.urls)),
     path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
     path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view()),
--- escola/views.py
+++ escola/views.py
@@ -3,6 +3,30 @@ from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+
+class LoginPage(View):
+    template_name = 'login/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('/admin/')  # Substitua 'pagina_inicial' pela sua URL desejada
+        else:
+            return render(request, self.template_name, {'error_message': 'Aguardando informações'})
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
